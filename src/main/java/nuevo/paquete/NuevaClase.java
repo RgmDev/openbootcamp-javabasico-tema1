13c6bc6477b21339e81ade0f1a711e92f409dbcd
rgonzalez <rgonzalez@mkdautomotive.com>
2022-06-27 14:50:23 +0200

    Initial commit

---
+++ src/main/java/nuevo/paquete/NuevaClase.java
@@ -0,0 +1,37 @@
+package nuevo.paquete;
+
+public class NuevaClase {
+    public static void main(String[] args) {
+
+        byte numberByte = 5;
+        System.out.println(numberByte);
+        
+        short numberShort = 10;
+        System.out.println(numberShort);
+
+        int numberInt = 20;
+        System.out.println(numberInt);
+        
+        long numberLong = 40;
+        System.out.println(numberLong);
+        
+        float numberFloat = 2.2f;
+        System.out.println(numberFloat);
+        
+        double numberDouble = 4.4d;
+        System.out.println(numberDouble);
+        
+        boolean isTrue = true;
+        System.out.println(isTrue);
+
+        boolean isFalse = false;
+        System.out.println(isFalse);
+
+        char varChar = 'a';
+        System.out.println(varChar);
+
+        String varString = "Variable tipo String";
+        System.out.println(varString);
+
+    }
+}
